440e0f1b7f3db45cdb8d4c253ad49314591015ec
Paweł Proc <pawel.henryk.proc@gmail.com>
2016-10-09 02:09:35 +0200

    InputData first test

--- src/test/java/pl/parser/nbp/InputDataTest.java
+++ src/test/java/pl/parser/nbp/InputDataTest.java
@@ -1,30 +1,48 @@
 package pl.parser.nbp;
 
+import org.testng.Assert;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import pl.parser.nbp.utils.Currency;
 
-import static org.testng.Assert.*;
+import java.time.LocalDate;
 
 /**
- * Created by Gosia on 09.10.2016.
+
+ Should parse valid dates and currency and provide proper objects
+
+ Should throw illegal argument exception for wrong dates order
+ Should throw illegal argument exception for unsupported currency
+ Should throw illegal argument exception for wrong date format
+ Should throw illegal argument exception for too old dates
+ Should throw illegal argument exception for wrong number of arguments
+
  */
 public class InputDataTest {
 
-    public static final String USD = Currency.USD.toString();
-
     @Test(dataProvider = "provideDates")
-    public void shouldParseValidDates(String from, String to){
+    public void shouldParseValidDates(String fromDateInput, String toDateInput, String currencyInput){
 
-        String [] args = {from, to, USD};
-        new InputData(args);
+        String [] args = {fromDateInput, toDateInput, currencyInput};
+        InputData inputData = new InputData(args);
 
+        final LocalDate fromDate = inputData.getFrom();
+        Assert.assertEquals(fromDate.toString(), fromDateInput, "Should return valid fromDate");
+
+        final LocalDate toDate = inputData.getTo();
+        Assert.assertEquals(toDate.toString(), toDateInput, "Should return valid toDate");
+
+        final String currency = inputData.getCurrency().toString();
+        Assert.assertEquals(currencyInput, currency, "Should return valid Currency");
     }
 
     @DataProvider
     public Object[][] provideDates(){
         return new Object[][]{
-                {"2015-01-01", "2016-02-10"}
+                {"2015-01-01", "2016-02-01", "USD"},
+                {"2015-01-02", "2016-01-01", "EUR"},
+                {"2014-01-03", "2016-02-07", "CHF"},
+                {"2011-01-01", "2016-02-06", "GBP"}
         };
     }
 
